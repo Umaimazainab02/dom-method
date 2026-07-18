69577404dec6815e3aa8b7b9f4c9efdff5f10a36
Umaimazainab02 <umaimazainab429@gmail.com>
2026-07-18 11:20:56 +0500

    Initial commit

---
+++ dome3.js
@@ -0,0 +1,44 @@
+
+// // btn1.addEventListener("click", () => {
+// //     btn2.textContent = "Umaima zainab";
+// //     document.getElementById("btn1").textContent = "JavaScript";
+// // })
+
+// // function change() {
+// //     let button1 = document.createElement("p");
+// //     button1.textContent = "Hello";
+
+// //     document.body.appendChild(button1);
+// //     button1.style.color="purple"
+// //     button1.style.fontSize="30px"
+// //     button1.style.text
+// // }
+
+// // function btn1(){
+// //     let btn2=document.createElement("h2");
+// //     btn2.textContent="beauty";
+// //     document.body.appendChild(btn2);
+// //     btn2.style.color="pink"
+// //     btn2.style.fontSize="100px"
+// //     setTimeout(() => {
+// //         btn2.remove();
+// //     }, 3000);
+
+// // }
+
+// function btn1(){
+//     let img=document.getElementById("img1");
+//     img.setAttribute("src","dog.jpg");
+// }
+// function link12(){
+//     let link=document.getElementById("link1")
+//     link.textContent="whatsapp"
+//     link.setAttribute("href","https://web.whatsapp.com/")
+//         // window.open("https://web.whatsapp.com/", "_blank");
+
+// }
+
+
+
+
+    
